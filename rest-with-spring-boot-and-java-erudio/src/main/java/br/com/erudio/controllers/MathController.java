4324d6253ae79ff6d276f4061b68ac7304ddac3d
Pedro Lucas Alves Batista <pedrolucasbatista26@gmail.com>
2025-11-23 20:57:08 -0300

    teste

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/MathController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/MathController.java
@@ -1,4 +1,99 @@
 package br.com.erudio.controllers;
 
 public class MathController {
+
+    @RequestMapping("/sum/{number1}/{number2}")
+    public Double sum(
+            @PathVariable("number1") String number1,
+            @PathVariable("number2") String number2) throws IllegalArgumentException {
+
+        if (!isNumber(number1) || !isNumber(number2)) {
+            throw new IllegalArgumentException();
+        }
+
+        return convertDouble(number1) + convertDouble(number2);
+    }
+
+    @RequestMapping("/subtr/{number1}/{number2}")
+    public Double subtr(
+            @PathVariable("number1") String number1,
+            @PathVariable("number2") String number2) throws IllegalArgumentException {
+
+        if (!isNumber(number1) || !isNumber(number2)) {
+            throw new IllegalArgumentException();
+        }
+
+        return convertDouble(number1) - convertDouble(number2);
+    }
+
+    @RequestMapping("/multi/{number1}/{number2}")
+    public Integer multi(
+            @PathVariable("number1") String number1,
+            @PathVariable("number2") String number2 ) throws IllegalArgumentException {
+
+        if (!isNumber(number1) || !isNumber(number2)) {
+            throw new IllegalArgumentException();
+        }
+
+        return convertInteger(number1) * convertInteger(number2);
+    }
+
+    @RequestMapping("/div/{number1}/{number2}")
+    public Double div(
+            @PathVariable("number1") String number1,
+            @PathVariable("number2") String number2) {
+
+        if (!isNumber(number1) || !isNumber(number2)) {
+            throw new IllegalArgumentException();
+        }
+
+        return convertDouble(number1) / convertDouble(number2);
+    }
+
+    @RequestMapping("/media/{number1}/{number2}")
+    public Double media(
+            @PathVariable("number1") String number1,
+            @PathVariable("number2") String number2) {
+
+        if (!isNumber(number1) || !isNumber(number2)) {
+            throw new IllegalArgumentException();
+        }
+
+        return (convertDouble(number1) + convertDouble(number2)) / 2;
+    }
+
+    @RequestMapping("/raiz/{number}")
+    public Integer raiz(
+            @PathVariable("number") String number) {
+        for (int i = 1; i < convertInteger(number); i++) {
+            if (i * i == convertInteger(number)) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    public boolean isNumber(String number) throws IllegalArgumentException {
+        if (number == null || number.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        String n = number.replace(",",".");
+        return n.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+
+    public Double convertDouble(String number) throws IllegalArgumentException {
+        if (number == null || number.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        return Double.parseDouble(number);
+    }
+
+    public Integer convertInteger(String number) throws IllegalArgumentException {
+        if (number == null || number.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        return Integer.parseInt(number);
+    }
+
 }

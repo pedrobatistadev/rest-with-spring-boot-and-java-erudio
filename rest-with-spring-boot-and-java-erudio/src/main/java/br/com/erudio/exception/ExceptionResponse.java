e110e3429e7308252579bff917effda16de458b6
Pedro Lucas Alves Batista <pedrolucasbatista26@gmail.com>
2025-11-23 23:18:23 -0300

    Teste

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exception/ExceptionResponse.java
@@ -0,0 +1,4 @@
+package br.com.erudio.exception;
+
+public record ExceptionResponse() {
+}
---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exception/UnsupportedMathOperationException.java
@@ -0,0 +1,7 @@
+package br.com.erudio.exception;
+
+public class UnsupportedMathOperationException extends RuntimeException {
+  public UnsupportedMathOperationException(String message) {
+    super(message);
+  }
+}

11fc0882840eea934ae3317cedb200ceb0ce2392
Pedro Lucas Alves Batista <pedrolucasbatista26@gmail.com>
2025-11-23 23:20:01 -0300

    .

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/MathController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/MathController.java
@@ -1,5 +1,12 @@
 package br.com.erudio.controllers;
 
+import br.com.erudio.exception.UnsupportedMathOperationException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
 public class MathController {
 
     @RequestMapping("/sum/{number1}/{number2}")

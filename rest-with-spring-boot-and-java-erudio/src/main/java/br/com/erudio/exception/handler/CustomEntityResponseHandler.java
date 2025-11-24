7a1c2ee5b00bf33db36041fcb8ca272ce69e18c6
Pedro Lucas Alves Batista <pedrolucasbatista26@gmail.com>
2025-11-23 23:19:17 -0300

    .

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exception/handler/CustomEntityResponseHandler.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exception/handler/CustomEntityResponseHandler.java
@@ -1,4 +1,20 @@
 package br.com.erudio.exception.handler;
 
 public class CustomEntityResponseHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleAllException(Exception ex, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UnsupportedOperationException.class)
+    public final ResponseEntity<ExceptionResponse> handleBadRequestException(Exception ex, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 }
+
+

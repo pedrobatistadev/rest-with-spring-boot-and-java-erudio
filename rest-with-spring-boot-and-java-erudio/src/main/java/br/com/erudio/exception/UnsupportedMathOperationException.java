5e2a6a8f0204fe58e95cd1bc1495055ca5b45d61
Pedro Lucas Alves Batista <pedrolucasbatista26@gmail.com>
2025-11-23 23:19:51 -0300

    .

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exception/UnsupportedMathOperationException.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exception/UnsupportedMathOperationException.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UnsupportedMathOperationException extends RuntimeException {
-  public UnsupportedMathOperationException(String message) {
-    super(message);
-  }
+
+    public UnsupportedMathOperationException(String message) {
+        super(message);
+    }
 }

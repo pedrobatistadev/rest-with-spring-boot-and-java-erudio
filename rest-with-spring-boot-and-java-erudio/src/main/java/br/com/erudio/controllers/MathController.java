b22ab8daa44ed30bfb47ce091e826c8a233f6f86
Pedro Lucas Alves Batista <pedrolucasbatista26@gmail.com>
2025-11-25 21:26:37 -0300

    .

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/MathController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/MathController.java
@@ -31,7 +31,7 @@ public class MathController {
             throw new UnsupportedMathOperationException("Insira um valor válido ! ");
         }
 
-        return convertDouble(number1) - convertDouble(number2);
+        return math.subtr(mathValidation.convertDouble(number1), mathValidation.convertDouble(number2));
     }
 
     @RequestMapping("/multi/{number1}/{number2}")

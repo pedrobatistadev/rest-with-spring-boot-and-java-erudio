9f8207536ed4f2973d904f484f76bb94ea0fb70b
Pedro Lucas Alves Batista <pedrolucasbatista26@gmail.com>
2025-11-25 21:25:15 -0300

    .

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/math/OperationMath.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/math/OperationMath.java
@@ -1,4 +1,40 @@
 package br.com.erudio.math;
 
 public class OperationMath {
+
+    public Double sum(Double number1, Double number2) {
+
+        return number1 + number2;
+    }
+
+    public Double subtr(Double number1, Double number2) {
+
+        return number1 - number2;
+    }
+
+    public Double multi(Double number1, Double number2) {
+
+        return number1 * number2;
+    }
+
+    public Double div(Double number1, Double number2) {
+
+        return number1 / number2;
+    }
+
+    public Double media(Double number1, Double number2) {
+
+        return (number1 + number2) / 2;
+    }
+
+    public int raiz(int number1) {
+
+        for (int i = 1; i < number1; i++) {
+            if (i * i == number1) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
 }

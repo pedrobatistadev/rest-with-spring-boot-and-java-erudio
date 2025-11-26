a204a65e8e2d0e4e58d752139de4cb9c275538ff
Pedro Lucas Alves Batista <pedrolucasbatista26@gmail.com>
2025-11-25 21:27:07 -0300

    .

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/validations/mathValidation.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/validations/mathValidation.java
@@ -1,4 +1,26 @@
 package br.com.erudio.validations;
 
 public class mathValidation {
+
+    public static boolean isNumber(String number) {
+        if (number == null || number.isEmpty()) {
+            throw new UnsupportedMathOperationException("Insira um valor válido !");
+        }
+        String n = number.replace(",",".");
+        return n.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+
+    public static Double convertDouble(String number) {
+        if (number == null || number.isEmpty()) {
+            throw new UnsupportedMathOperationException("Insira um valor válido !");
+        }
+        return Double.parseDouble(number);
+    }
+
+    public static Integer convertInteger(String number) {
+        if (number == null || number.isEmpty()) {
+            throw new UnsupportedMathOperationException("Insira um valor válido !");
+        }
+        return Integer.parseInt(number);
+    }
 }

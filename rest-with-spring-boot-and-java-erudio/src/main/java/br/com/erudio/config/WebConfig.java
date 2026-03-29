45aca88c9ab91ee7e4128253d3e96f82f934aaa3
Pedro Lucas Alves Batista <pedrolucasbatista26@gmail.com>
2026-03-29 17:36:05 -0300

    implementando CORS

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
@@ -1,14 +1,29 @@
 package br.com.erudio.config;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.print.attribute.standard.Media;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.originPatterns}")
+    private String corsOriginPatterns;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String[] allowed = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedOrigins(allowed)
+                .allowedMethods("POST", "GET", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {

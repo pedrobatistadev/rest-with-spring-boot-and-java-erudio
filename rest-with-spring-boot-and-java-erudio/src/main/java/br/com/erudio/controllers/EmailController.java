baa4320609b6f83fb5b43640e011facc3242d481
Pedro Lucas Alves Batista <pedrolucasbatista26@gmail.com>
2026-09-09 00:08:36 -0300

    Envio de E-mail com anexo implementado.

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/EmailController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/EmailController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -20,15 +17,18 @@ public class EmailController implements EmailControllerDocs {
     @Autowired
     private EmailService service;
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping
     @Override
     public ResponseEntity<String> sendEmail(@RequestBody EmailRequestDTO emailRequest) {
         service.sendSimpleEmail(emailRequest);
         return ResponseEntity.status(HttpStatus.OK).body("Email sent with sucess!");
     }
 
+    @PostMapping(value = "/withAttachment", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Override
-    public ResponseEntity<String> sendEmailWithAttachment(String emailRequestJson, MultipartFile multipartFile) {
-        return null;
+    public ResponseEntity<String> sendEmailWithAttachment(@RequestParam("emailRequest") String emailRequest,
+                                                          @RequestParam("attachment") MultipartFile attachment) {
+        service.sendEmailWithAttachment(emailRequest,attachment);
+        return ResponseEntity.ok("e-mail with attachment sent successfully!");
     }
 }

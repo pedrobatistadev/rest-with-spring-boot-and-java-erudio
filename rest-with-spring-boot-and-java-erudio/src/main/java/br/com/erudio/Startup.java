71c80cabe2cfc6e62acd8c2579b47dd2eaedc8cd
Pedro Lucas Alves Batista <pedrolucasbatista26@gmail.com>
2026-09-22 23:24:58 -0300

    Continuação JWT

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Startup.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Startup.java
@@ -2,6 +2,12 @@ package br.com.erudio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class Startup {
@@ -9,6 +15,23 @@ public class Startup {
 	public static void main(String[] args) {
 
         SpringApplication.run(Startup.class, args);
+
+        PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder(
+                "",
+                8,
+                185000,
+                Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA1);
+
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("pbkdf2", pbkdf2Encoder);
+        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+
+        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
+        var pass1 = passwordEncoder.encode("admin123");
+        var pass2 = passwordEncoder.encode("admin234");
+
+        System.out.println(pass1);
+        System.out.println(pass2);
 
     }
 
